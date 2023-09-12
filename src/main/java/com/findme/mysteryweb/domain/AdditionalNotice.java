03ef2e1e5c9e7f8f097951b83c0a82c7d05a56c6
WAME-LEL <fnskh0606@gmail.com>
2023-09-13 00:23:46 +0900

    domain developing

--- src/main/java/com/findme/mysteryweb/domain/AdditionalNotice.java
+++ src/main/java/com/findme/mysteryweb/domain/AdditionalNotice.java
@@ -2,4 +2,6 @@ package com.findme.mysteryweb.domain;
 
 
 public class AdditionalNotice {
+
+
 }
---
+++ src/main/java/com/findme/mysteryweb/domain/Member.java
@@ -0,0 +1,24 @@
+package com.findme.mysteryweb.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+    private String identification;
+    private String password;
+
+}
